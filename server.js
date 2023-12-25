d70165cbd37f69eed5d106525ac9786354cb9b3a
Harshil0309 <harshilgupta562@gmail.com>
2023-12-25 16:54:39 +0530

    Some changes

--- server.js
+++ server.js
@@ -120,13 +120,6 @@ app.get("/allid", async (req, res) => {
     }
   });
 
-  if (process.env.NODE_ENV === "production") {
-    server.use("/", express.static("client/build"));
-  
-    server.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-    });
-  }
   
 
 // Start the server
